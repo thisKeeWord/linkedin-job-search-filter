d76d4d0e1195b364697161aea987a1ccfb9de639
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2018-07-01 20:36:11 -0700

    works on intervals

--- app/scripts.babel/content_script.js
+++ app/scripts.babel/content_script.js
@@ -1,9 +1,10 @@
-window.onload = function() {
+// window.onload = function() {
+setInterval(function() {
 	var companyList = $('.job-card-search__company-name-link');
 	for (var i = 0; i < companyList.length; i++) {
 		if ($(companyList[i]).children()[0].innerText === 'VirtualVocations' || $(companyList[i]).children()[0].innerText === 'CyberCoders') {
 			$(companyList[i]).parent().parent().parent().parent().parent().remove();
 		}
 	}
-
-}
+}, 2000);
+// }
