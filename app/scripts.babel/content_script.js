c182b00a79ae99f8c262521bbc840cea30aa930b
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2018-07-06 22:28:43 -0700

    adding functionality to add companies in filter list

--- app/scripts.babel/content_script.js
+++ app/scripts.babel/content_script.js
@@ -1,10 +1,17 @@
 // window.onload = function() {
-setInterval(function() {
-	var companyList = $('.job-card-search__company-name-link');
-	for (var i = 0; i < companyList.length; i++) {
-		if ($(companyList[i]).children()[0].innerText === 'VirtualVocations' || $(companyList[i]).children()[0].innerText === 'CyberCoders') {
-			$(companyList[i]).parent().parent().parent().parent().parent().remove();
-		}
-	}
-}, 0000);
+// setInterval(function() {
+// 	var companyList = $('.job-card-search__company-name-link');
+// 	console.log("akdfg")
+// 	for (var i = 0; i < companyList.length; i++) {
+// 		if ($(companyList[i]).children()[0].innerText === 'VirtualVocations' || $(companyList[i]).children()[0].innerText === 'CyberCoders') {
+// 			$(companyList[i]).parent().parent().parent().parent().parent().remove();
+// 		}
+// 	}
+// }, 0000);
 // }
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+	// Handle message.
+	// In this example, message === 'whatever value; String, object, whatever'\
+	console.log(message.data, sender, 'as;dif')
+});
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,17 +1,29 @@
 'use strict';
 
 
+// if user added company twice, doesn't handle
+var companyList = [];
 var form = document.getElementById("companyFilter");
 form.addEventListener("submit", function(e) {
 	e.preventDefault();
-	var input = document.getElementById('input-box');
-
-	console.log(e.target.value, 'hello')
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {data: input.value}, function(response) {
-			$('#status').html('changed data in page');
-			console.log('success');
+	var input = document.getElementById('input-box').value.split(", ");
+	console.log(input, 'input')
+	if (input !== null) {
+		companyList = companyList.concat(input);
+		console.log('first list', companyList)
+		chrome.storage.sync.get(['key'], function(result) {
+			// console.log(typeof result.key)
+			if (result.key !== undefined || result.key !== null) {
+				companyList = companyList.concat(result.key);
+				console.log(companyList)
+			}
+			chrome.storage.sync.set({key: companyList}, function() {
+				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+					chrome.tabs.sendMessage(tabs[0].id, {data: result}, function(response) {
+						console.log('success');
+					});
+				});
+			});
 		});
-	});
-	
+	}
 });
