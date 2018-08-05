5ea2e65be865396c741d05f5530e10521b603bdf
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2018-08-05 02:29:13 -0700

    update to logic in content_script and comments

--- app/scripts.babel/content_script.js
+++ app/scripts.babel/content_script.js
@@ -14,4 +14,14 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	// Handle message.
 	// In this example, message === 'whatever value; String, object, whatever'\
 	console.log(message.data, sender, 'as;dif')
+	// window.onload = function() {
+		setInterval(function() {
+			var companyList = $('.job-card-search__company-name-link');
+			for (var i = 0; i < companyList.length; i++) {
+				if ($(companyList[i]).children()[0].innerText === 'VirtualVocations' || $(companyList[i]).children()[0].innerText === 'CyberCoders' || $(companyList[i]).children()[0].innerText === 'Apple') {
+					$(companyList[i]).parent().parent().parent().parent().parent().remove();
+				}
+			}
+		}, 0000);
+	// }
 });
